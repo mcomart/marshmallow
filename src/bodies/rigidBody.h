9dff93d6be2c29a206343a62b66d02d10684d3aa
Marina CM <mcomart@gmail.com>
2020-02-26 20:19:23 +0100

    added first header

---
+++ src/bodies/rigidBody.h
@@ -0,0 +1,28 @@
+#ifndef RIGIDBODY_H
+#define RIGIDBODY_H
+
+#include "../utils/math/marshmath.h"
+
+class RigidBody {
+public:
+    /* Constant quantities */
+    double mass;
+    Matrix3 IBody;
+    Matrix3 IBodyInv;
+
+    /* State variables */
+    Vector3 x;
+    Matrix3 R;
+    Vector3 P;
+    Vector3 L;
+
+    /* Derived quantities (auxiliary) */
+    Matrix3 Iinv;
+    Vector3 v;
+    Vector3 omega;
+
+    /* Computed quantities */
+    Vector3 force;
+    Vector3 torque;
+};
+#endif
